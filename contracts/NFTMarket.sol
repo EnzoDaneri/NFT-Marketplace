cd83e1d6c59eb8bb059f63174a9c65754ce595e4
EnzoDaneri <enzodaneri@hotmail.com>
2022-04-19 16:36:51 -0300

    added functions on create Market Item

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -10,4 +10,49 @@ contract NFTMarket is ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private _itemIds;
     Counters.Counter private _itemsSold;
+
+    address payable owner;
+    uint256 listingPrice = 0.025 ether;
+
+    constructor() {
+     owner = payable(msg.sender);
+    }
+
+    struct MarketItem {
+        uint itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+    
+    mapping(uint256 => MarketItem) private idMarketItem;
+    
+    event MarketItemCreated (
+        uint indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner, 
+        uint256 price,
+        bool sold
+
+
+    );
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function createMarketItem(
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant {
+        require(price > 0, "Price must be at least 1 wei");
+        require(msg.value == listingPrice, "Price must ve equal to listing price");
+    }
+
 }
